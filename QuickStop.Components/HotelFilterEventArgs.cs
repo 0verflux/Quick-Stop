42d621452378abe9885a9337534e5cbbf6068072
FDemin <francisdemin@hotmail.com>
2020-07-26 01:54:04 +0800

    Added Event Arguments for Hotel Filter and Selected Hotels

---
+++ QuickStop.Components/HotelFilterEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using QuickStop.Domain.Enums;
+using System;
+
+namespace QuickStop.Components
+{
+    public class HotelFilterEventArgs : EventArgs
+    {
+        public Location Location { get; private set; }
+        public int GuestCount { get; private set; }
+        public bool SortMode { get; private set; }
+
+        public HotelFilterEventArgs(Location location, int guestCount, bool sortMode) : base()
+        {
+            Location = location;
+            GuestCount = guestCount;
+            SortMode = sortMode;
+        }
+    }
+}
