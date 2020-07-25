7c5c9524c7a6061d24411c9c9531815294087cd2
FDemin <francisdemin@hotmail.com>
2020-07-26 02:26:43 +0800

    Added Hotel Details View and Presenter File

---
+++ QuickStop.Client/Presenters/HotelDetailsPresenter.cs
@@ -0,0 +1,25 @@
+﻿using QuickStop.Client.Base;
+using QuickStop.Client.Views;
+using QuickStop.Domain.Models;
+using QuickStop.Infrastructure.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickStop.Client.Presenters
+{
+    public sealed class HotelDetailsPresenter : PresenterBase<IHotelDetailsView>
+    {
+        private Hotel selectedHotel;
+        private readonly IHotelRepository hotelRepository;
+
+        public HotelDetailsPresenter(IHotelDetailsView hotelDetailsView) : base(hotelDetailsView)
+        {
+
+        }
+        
+
+    }
+}
---
+++ QuickStop.Client/Views/IHotelDetailsView.cs
@@ -0,0 +1,19 @@
+﻿using QuickStop.Client.Contracts;
+using QuickStop.Components;
+using QuickStop.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickStop.Client.Views
+{
+    public interface IHotelDetailsView : IView
+    {
+        event EventHandler HotelSelected;
+
+        void DisplayHotel(Hotel hotel);
+        void ViewReservationDetails(Hotel hotel);
+    }
+}
