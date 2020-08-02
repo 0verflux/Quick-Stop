f3e5562c66859d6a1d28ea20458dad9b11439d2c
FDemin <francisdemin@hotmail.com>
2020-08-02 16:11:34 +0800

    Renamed Method to CalculateTotalCost

--- QuickStop.Components/Helpers/HotelRoomBooking.cs
+++ QuickStop.Components/Helpers/HotelRoomBooking.cs
@@ -8,7 +8,7 @@ namespace QuickStop.Components.Helpers
     public static class HotelRoomBooking
     {
         /// <summary>
-        /// Calculates the Total Price of a <see cref="Domain.Models.HotelBook"/>.
+        /// Calculates the Total Cost of a <see cref="Domain.Models.HotelBook"/>.
         /// </summary>
         /// <param name="hotelPrice">The Current <see cref="Domain.Models.HotelRoom"/> Price.</param>
         /// <param name="guestCount">The Amount of Guests in the <see cref="Domain.Models.HotelBook"/>.</param>
@@ -17,7 +17,7 @@ namespace QuickStop.Components.Helpers
         /// <param name="checkIn">The check-in date.</param>
         /// <param name="checkOut">The check=out date.</param>
         /// <returns>A Total Cost to Book a <see cref="Domain.Models.HotelRoom"/>.</returns>
-        public static decimal CalculateTotalPrice(decimal hotelPrice, int guestCount, int minGuestCount, int maxGuestCount, DateTime checkIn, DateTime checkOut)
+        public static decimal CalculateTotalCost(decimal hotelPrice, int guestCount, int minGuestCount, int maxGuestCount, DateTime checkIn, DateTime checkOut)
         {
             return hotelPrice * (1 + ((decimal)(guestCount - minGuestCount) / (maxGuestCount - minGuestCount))) * (decimal)Math.Round((checkOut - checkIn).TotalDays);
         }
